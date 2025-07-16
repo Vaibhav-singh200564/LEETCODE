8fb5609322ed767be141e996bcfcb1d713f7ed23
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-07-16 21:37:49 +0530

    Time: 3 ms (15.71%) | Memory: 9.7 MB (58.36%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        set<pair<int, int>> visited;
+        int x = 0, y = 0;
+        visited.insert({x, y});
+        
+        for (char dir : path) {
+            if (dir == 'N') y++;
+            else if (dir == 'S') y--;
+            else if (dir == 'E') x++;
+            else if (dir == 'W') x--;
+
+            if (visited.count({x, y})) {
+                return true; 
+            }
+
+            visited.insert({x, y});
+        }
+
+        return false;
+    }
+};
