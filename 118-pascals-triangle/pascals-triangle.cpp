faff317a079777e2182fca0df2255af729fa9091
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-08-01 23:07:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (62.59%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> v;
-
+        vector<vector<int>> triangle;
         for (int i = 0; i < numRows; i++) {
-            // Initialize each row with size i + 1
-            vector<int> a(i + 1, 1);  // All elements initialized to 1
-            
-            // Fill the inner elements of the row (if applicable)
+            vector<int> row(i + 1, 1);
             for (int j = 1; j < i; j++) {
-                a[j] = v[i - 1][j - 1] + v[i - 1][j];
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
             }
-
-            v.push_back(a);  // Add the row to the result
+            triangle.push_back(row);
         }
-
-        return v;
+        return triangle;
     }
 };
