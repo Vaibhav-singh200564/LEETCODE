d4563d723393175c61c626edd81895823cd359d6
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-08-05 21:25:16 +0530

    Time: 66 ms (10.68%) | Memory: 78.4 MB (8.03%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
-        int i = 0;
-        while (i < n) {
-            if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
-                swap(nums[i], nums[nums[i] - 1]);
-            } else {
-                i++;
+        unordered_set<int> s;
+        for(int num:nums){
+            if(num>0){
+                s.insert(num);
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1) return i + 1;
+        for(int i=1; i<=nums.size(); i++){
+            if(s.find(i)==s.end()){
+                return i;
+            }
         }
-        return n + 1;
+        return nums.size()+1;
     }
 };
