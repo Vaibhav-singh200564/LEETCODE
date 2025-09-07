83fa618c454095b0a94699aace3c36871c2f5e5b
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-09-07 23:39:17 +0530

    Time: 21 ms (94.31%) | Memory: 68.3 MB (69.37%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b){
-            return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
-        });
-        vector<vector<int>> result(points.begin(), points.begin() + k);
+        vector<pair<int,int>> dist; 
+        for(int i = 0; i < points.size(); i++){
+            int d = points[i][0]*points[i][0] + points[i][1]*points[i][1];
+            dist.push_back({d, i});
+        }
+        
+        sort(dist.begin(), dist.end());
+        
+        vector<vector<int>> result;
+        for(int i = 0; i < k; i++){
+            result.push_back(points[dist[i].second]);
+        }
         return result;
     }
 };
