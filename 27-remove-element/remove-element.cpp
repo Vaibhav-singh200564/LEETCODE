2320af460b722bd524461255e26d9aa09d543b8f
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-10-16 11:50:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (10.13%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,15 +1,22 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k = 0; 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != val) {
-                nums[k++] = nums[i];
+        stack<int> st;
+
+        // Push only elements not equal to val
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val){
+                st.push(nums[i]);
             }
         }
-        return k; 
+
+        // Restore elements to nums in correct order
+        int k = st.size(); // number of valid elements
+        for(int i = k - 1; i >= 0; i--){
+            nums[i] = st.top();
+            st.pop();
+        }
+
+        return k; // return new length
     }
 };
