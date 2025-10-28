7ab24f194b7cbf3442c87fa892e2b9a521a76d10
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-10-28 18:18:36 +0530

    Time: 15 ms (87.50%) | Memory: 80.5 MB (86.01%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
+    int largestRectangleArea(vector<int>& arr) {
+        int n = arr.size();
+        int nsi[n];
         stack<int> st;
-        int maxArea = 0;
-        for (int i = 0; i <= n; ++i) {
-            int h = (i == n ? 0 : heights[i]);
-            while (!st.empty() && heights[st.top()] >= h) {
-                int height = heights[st.top()];
+
+        // Next Smaller Index (NSI)
+        nsi[n - 1] = n;
+        st.push(n - 1);
+        for (int i = n - 2; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
                 st.pop();
-                int leftIndex = (st.empty() ? 0 : st.top() + 1);
-                int width = i - leftIndex;
-                maxArea = max(maxArea, height * width);
             }
+            nsi[i] = st.empty() ? n : st.top();
             st.push(i);
         }
+
+        // Previous Smaller Index (PSI)
+        int psi[n];
+        stack<int> gt;
+        psi[0] = -1;
+        gt.push(0);
+        for (int i = 1; i < n; i++) {
+            while (!gt.empty() && arr[gt.top()] >= arr[i]) {
+                gt.pop();
+            }
+            psi[i] = gt.empty() ? -1 : gt.top();
+            gt.push(i);
+        }
+
+        // Calculate max area
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int height = arr[i];
+            int breadth = nsi[i] - psi[i] - 1;
+            int area = height * breadth;
+            maxArea = max(maxArea, area);
+        }
+
         return maxArea;
     }
 };
