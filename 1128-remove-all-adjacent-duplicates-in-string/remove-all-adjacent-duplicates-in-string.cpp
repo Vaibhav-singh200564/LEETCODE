342b1e9067973af25cfb9a053985192e18bf901e
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-10-29 18:43:48 +0530

    Time: 7 ms (48.69%) | Memory: 14.3 MB (26.41%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        if(s.empty()) return "";
+        stack<char> st;
+        st.push(s[0]);
+        for(int i = 1; i < s.length(); i++){
+            if(!st.empty() && s[i] == st.top()) {
+                st.pop();
+            } else {
+                st.push(s[i]);
+            }
+        }
+        string result = "";
+        while(!st.empty()){
+            result += st.top();
+            st.pop();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
