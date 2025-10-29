f51830ffc9fe3c3f4aa1b8739eb80ed10210dd00
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-10-29 18:52:20 +0530

    Time: 451 ms (13.96%) | Memory: 783.8 MB (6.00%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -4,19 +4,21 @@ public:
         if(s.empty()) return "";
         stack<char> st;
         st.push(s[0]);
-        for(int i = 1; i < s.length(); i++){
-            if(!st.empty() && s[i] == st.top()) {
+        for(int i=1; i<s.length(); i++){
+            if( !st.empty()  && s[i]==st.top()){
                 st.pop();
-            } else {
+            }
+            else{
                 st.push(s[i]);
             }
         }
-        string result = "";
-        while(!st.empty()){
-            result += st.top();
+        string result="";
+        while(st.size()>0){
+            result=result+st.top();
             st.pop();
         }
-        reverse(result.begin(), result.end());
+        reverse(result.begin(),result.end());
         return result;
+        
     }
 };
