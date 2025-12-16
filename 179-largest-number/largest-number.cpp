3ffa2c77e7218a8f2e7def14941b965d45fb0944
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-12-16 23:30:41 +0530

    Time: 4 ms (51.79%) | Memory: 17 MB (45.44%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,12 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool compare(string s1, string s2) {
+    if (s1 + s2 > s2 + s1)
+        return true;   // s1 pehle aayega
+    else
+        return false;  // s2 pehle aayega
+}
+
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
         vector<string> arr;
         for (int num : nums) arr.push_back(to_string(num));
 
-        sort(arr.begin(), arr.end(), [](string &s1, string &s2) {
-            return s1 + s2 > s2 + s1;
-        });
+        sort(arr.begin(), arr.end(), compare);  // normal function use
 
         if (arr[0] == "0") return "0";
 
