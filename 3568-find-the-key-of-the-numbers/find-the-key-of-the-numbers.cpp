4f0c57edffe48078a009ef4dd817ee8690290c61
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-12-17 23:24:53 +0530

    Time: 1 ms (19.84%) | Memory: 10.1 MB (21.83%) - LeetSync

--- 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.cpp
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int generateKey(int num1, int num2, int num3) {
 
-        // STEP 1: convert numbers to 4-digit strings
         string a = to_string(num1);
         string b = to_string(num2);
         string c = to_string(num3);
@@ -11,14 +10,20 @@ public:
         while (b.size() < 4) b = "0" + b;
         while (c.size() < 4) c = "0" + c;
 
-        // STEP 2: find min digit at each position
         string result = "";
+
         for (int i = 0; i < 4; i++) {
-            char mn = min(a[i], min(b[i], c[i]));
-            result += mn;
+            char smallest = a[i];   // assume a[i] is smallest
+
+            if (b[i] < smallest)
+                smallest = b[i];
+
+            if (c[i] < smallest)
+                smallest = c[i];
+
+            result += smallest;
         }
 
-        // STEP 3: convert to int (leading zeros removed automatically)
         return stoi(result);
     }
 };
