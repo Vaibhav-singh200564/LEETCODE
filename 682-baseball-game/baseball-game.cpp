ef57b7c952828b7d9e0cd715d483a57186f2e91a
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-12-23 22:09:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (12.98%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int> st;
+
+        for (string op : operations) {
+            if (op == "C") {
+                st.pop();
+            } 
+            else if (op == "D") {
+                st.push(st.top() * 2);
+            } 
+            else if (op == "+") {
+                int top1 = st.top(); st.pop();
+                int top2 = st.top();
+                st.push(top1);              // push back
+                st.push(top1 + top2);
+            } 
+            else {
+                st.push(stoi(op));          // convert string to int
+            }
+        }
+
+        int sum = 0;
+        while (!st.empty()) {
+            sum += st.top();
+            st.pop();
+        }
+
+        return sum;
+    }
+};
