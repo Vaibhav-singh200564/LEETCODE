76fec949a425f80fe759ae17d1b9bc916dbfecf8
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2025-12-25 22:29:30 +0530

    Time: 26 ms (68.29%) | Memory: 52 MB (28.90%) - LeetSync

---
+++ 924-fair-candy-swap/fair-candy-swap.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+        int sumA = 0, sumB = 0;
+        for (int x : aliceSizes) sumA += x;
+        for (int y : bobSizes) sumB += y;
+        
+        int diff = (sumB - sumA) / 2;
+        
+        unordered_set<int> setB(bobSizes.begin(), bobSizes.end());
+        
+        for (int x : aliceSizes) {
+            int y = x + diff;
+            if (setB.count(y)) {
+                return {x, y};
+            }
+        }
+        
+        return {}; // Should never reach here
+    }
+};
