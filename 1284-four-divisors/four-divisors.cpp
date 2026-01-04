287c876c2b089f931dd05e1f3e3f143a0a27d0f2
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-01-04 23:01:02 +0530

    Time: 5 ms (88.42%) | Memory: 16.1 MB (92.61%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+
+        for (int n : nums) {
+            int cnt = 0, sum = 0;
+
+            for (int i = 1; i * i <= n; i++) {
+                if (n % i == 0) {
+                    int d1 = i;
+                    int d2 = n / i;
+
+                    if (d1 == d2) {
+                        cnt++;
+                        sum += d1;
+                    } else {
+                        cnt += 2;
+                        sum += d1 + d2;
+                    }
+                }
+                if (cnt > 4) break;
+            }
+
+            if (cnt == 4)
+                ans += sum;
+        }
+
+        return ans;
+    }
+};
