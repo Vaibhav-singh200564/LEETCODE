7f3c24281ae88047b7b2bf0333d470bf88e7a24f
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-01-24 09:55:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (45.84%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.cpp
+++ 1542-consecutive-characters/consecutive-characters.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxPower(string s) {
-        int count = 1;
+        // int count = 1;
         int maxCount = 1;
 
         for (int i = 0; i < s.size(); i++) {
-            count = 1;   // reset for each new i
+           int  count = 1;   // reset for each new i
             for (int j = i + 1; j < s.size(); j++) {
                 if (s[i] == s[j]) {
                     count++;
