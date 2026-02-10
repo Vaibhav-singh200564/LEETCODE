a086d0946121ef55c61843ad84d083c0ee5ff2dc
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-02-10 11:12:53 +0530

    Time: 17 ms (6.35%) | Memory: 145.2 MB (7.71%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> pos, neg;
-
-        // Separate positives and negatives
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0) pos.push_back(nums[i]);
-            else neg.push_back(nums[i]);
-        }
-
+        int n=nums.size();
+        vector<int> pos;
+        vector<int> neg;
+        for(int i=0; i<n; i++){
+            if(nums[i]>0){
+                pos.push_back(nums[i]);
+            }else{
+                neg.push_back(nums[i]);
+            }
+        } 
         vector<int> result;
-
-        // Merge alternately using push_back
-        for (int i = 0; i < pos.size(); i++) {
-            result.push_back(pos[i]); // add positive
-            result.push_back(neg[i]); // add negative
-        }
-
+        for(int i=0; i<pos.size(); i++){
+            result.push_back(pos[i]);
+            result.push_back(neg[i]);
+        }       
         return result;
     }
 };
