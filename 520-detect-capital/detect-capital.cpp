db361bb9661dffb3c11d00435f4b92ecfb635eb1
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-02-12 16:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (4.29%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int upper = 0;
-        for (char c : word) {
-            if (isupper(c)) upper++;
-        }
-        return upper == word.size() || upper == 0 || (upper == 1 && isupper(word[0]));
+        string lower = word;
+        string upper = word;
+
+        // convert whole word to lowercase and uppercase
+        for (auto &c : lower) c = tolower(c);
+        for (auto &c : upper) c = toupper(c);
+
+        // case 1: all lowercase, case 2: all uppercase
+        if (word == lower || word == upper) return true;
+
+        // case 3: only first letter uppercase
+        if (isupper(word[0]) && word.substr(1) == lower.substr(1)) return true;
+
+        return false;
     }
 };
