3ae04b33495bb4b9f5e2c6f03e5bef5d9e00bf2e
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-02-13 00:08:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (5.22%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int length = 0;
-        int i = s.size() - 1;
-        
-        // Ignore trailing spaces
-        while (i >= 0 && s[i] == ' ') {
-            i--;
-        }
-        
-        // Count the length of the last word
-        while (i >= 0 && s[i] != ' ') {
-            length++;
-            i--;
-        }
-        
-        return length;
+        stringstream ss(s);
+        string word;
+
+        while (ss >> word);
+
+        return word.length();
     }
 };
