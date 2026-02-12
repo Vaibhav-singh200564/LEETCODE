8f1afb083cd09feb77208ec2d766125f6ea72628
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-02-12 23:57:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (33.41%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -1,19 +1,18 @@
-#include <set>
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        set<char> lower, upper;
-
-        for (char c : word) {
-            if (islower(c)) lower.insert(c);
-            else upper.insert(c);
+        set<char> lower,upper;
+        for(char c:word){
+            if(islower(c)){
+                lower.insert(c);
+            }else{
+                upper.insert(c);
+            }
         }
-
-        int count = 0;
-        for (char c : lower) {
-            if (upper.count(toupper(c))) count++; // check if uppercase exists
+        int count=0;
+        for(char c:lower){
+            if(upper.count(toupper(c))) count++;
         }
-
         return count;
     }
 };
