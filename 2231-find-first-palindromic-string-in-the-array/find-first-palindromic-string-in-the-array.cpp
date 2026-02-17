a426b2a5811d8e24152a5803dc6dfacc079eac45
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-02-18 00:03:22 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (86.49%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,13 +1,40 @@
+// class Solution {
+// public:
+//     string firstPalindrome(vector<string>& words) {
+//         for(string word:words){
+//             string rev=word;
+//             reverse(rev.begin(),rev.end());
+//             if(rev==word){
+//                 return word;
+//             }
+//             // return "";
+//         }
+//         return "";
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
+    bool isPalindrome(string &word) {
+        int i = 0, j = word.length() - 1;
+
+        while (i < j) {
+            if (word[i] != word[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
     string firstPalindrome(vector<string>& words) {
-        for(string word:words){
-            string rev=word;
-            reverse(rev.begin(),rev.end());
-            if(rev==word){
-                return word;
-            }
-            // return "";
+        for (string &word : words) {
+            if (isPalindrome(word))
+                return word;   // FIRST palindrome → return immediately
         }
         return "";
     }
