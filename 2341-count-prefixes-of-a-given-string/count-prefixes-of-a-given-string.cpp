cdfc4e329a3c3f64ea138473f764a580457ca3e2
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-02-25 12:14:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (32.25%) - LeetSync

---
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countPrefixes(vector<string>& words, string s) {
+        int count=0;
+        for(string word:words){
+            if(s.find(word)==0)
+            count++;
+        }
+        return count;
+    }
+};
