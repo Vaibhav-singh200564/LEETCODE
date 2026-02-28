51653fe8c230a65c4a5bb550f26c1c7611e9f663
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-02-28 23:56:17 +0530

    Time: 41 ms (78.86%) | Memory: 54 MB (66.43%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -1,9 +1,7 @@
 var reduce = function(nums, fn, init) {
     let accumulator = init;
-
     for (let i = 0; i < nums.length; i++) {
         accumulator = fn(accumulator, nums[i]);
     }
-
     return accumulator;
 };
