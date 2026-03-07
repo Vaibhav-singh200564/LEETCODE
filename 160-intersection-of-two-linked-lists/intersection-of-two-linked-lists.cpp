99273493636e4202061172e8742d2a5cb871899f
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-03-07 15:39:12 +0530

    Time: 57 ms (55.69%) | Memory: 23.9 MB (84.37%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -25,7 +25,7 @@ public:
                 tempA=tempA->next;
                 tempB=tempB->next;
             }
-            return tempA;
+            return tempB;
         }
         else{
             int diffB=lenB-lenA;
