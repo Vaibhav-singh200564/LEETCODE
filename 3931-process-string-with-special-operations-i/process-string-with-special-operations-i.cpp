170e3dd69f8cf53e2de6b995d5a70f2ed9a44739
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-06-16 21:27:18 +0530

    Time: 3 ms (76.89%) | Memory: 39.6 MB (87.50%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string processStr(string s) {
         string result = "";
-
         for(char ch : s) {
             if(ch >= 'a' && ch <= 'z') {
                 result += ch;
