b863f2d212ce3205f3cdfadaf07ff5164374f01e
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-08-20 15:56:45 +0530

    Time: 8 ms (6.61%) | Memory: 25.8 MB (33.02%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-string reverse(string s){
-    string ans="";
-    for(int i=s.size()-1; i>=0; i--){
-        ans+=s[i];
-    }
-    return ans;
-}
     int maximumNumberOfStringPairs(vector<string>& words) {
         int n=words.size();
+        unordered_set<string> s;
         int count=0;
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                string rev=reverse(words[i]);
-                if(rev==words[j]) count++;
+            s.insert(words[i]);
+        }
+        for(int i=0; i<n; i++){
+            string rev=words[i];
+            reverse(rev.begin(),rev.end());
+            if(words[i]==rev) continue;
+            if(s.find(rev)!=s.end()){
+                count++;
+                s.erase(words[i]);
             }
         }
         return count;
-        
     }
 };
