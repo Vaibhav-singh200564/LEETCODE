cad571d0131609832dd3e981365bce46e75c7038
Vaibhav-singh200564 <vaibhavsingh200564@gmail.com>
2026-08-31 22:00:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (81.89%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.cpp
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int maxSum = 0;
-
         for (int i = 0; i < accounts.size(); i++) {
-            int sum = 0;  
-
+            int sum = 0;
             for (int j = 0; j < accounts[i].size(); j++) {
                 sum += accounts[i][j];
             }
 
-            if (sum > maxSum) {
-                maxSum = sum;
-            }
+            maxSum = max(maxSum, sum);
         }
 
         return maxSum;
